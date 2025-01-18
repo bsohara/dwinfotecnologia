6d7054535335aac5a98c791fc487b58b8c7a0d95
bsohara <b.sohara.git@gmail.com>
2025-01-18 11:58:38 -0300

    recriando o projeto = erros no projeto anterior

---
+++ projeto-dwinfo2/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- Your content goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Your styles go here */
+</style>
